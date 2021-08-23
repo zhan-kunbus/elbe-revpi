d5bc4df388bb074463e743b8bcd423c246d6bc10
Zhi Han <z.han@kunbus.com>
2021-08-23 16:22:11 +0200

    Make the build script use preprocess
    Signed-off-by: Zhi Han <z.han@kunbus.com>

--- build.sh
+++ build.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-sudo echo
 
-if [ "1" != "$(mount | grep -c /mnt/wshare)" ]; then
-	sudo mkdir -p /mnt/wshare
-	sudo mount.cifs -o username=z.han,domain=KUNBUS //192.168.181.197/share/Entwickler/Simon /mnt/wshare
-fi
+BDIR="/var/cache/elbe/d6837082-6eae-4cc3-a6bf-a1bffd9f4928"
 
-git clean -f
+elbe preprocess armhf-revpi.xml
 
-BDIR=$(elbe control create_project)
-
-elbe control set_xml "$BDIR" ./pre-build.xml
-elbe control build "$BDIR"
-elbe control wait_busy "$BDIR"
-
-elbe control set_xml "$BDIR" ./armhf-revpi.xml
+elbe control set_xml "$BDIR" ./preprocess.xml
 elbe control build "$BDIR"
 elbe control wait_busy "$BDIR"
 
 elbe control get_file "$BDIR" armhf-revpi.img.tar.gz
 elbe control get_file "$BDIR" log.txt
 
-sudo mv armhf-revpi.img.tar.gz "/mnt/wshare/elbe-images/elbe-$(date +'%Y-%m-%d.%H')-revpi-buster.img.tar.gz"
-sudo mv log.txt "/mnt/wshare/elbe-images/elbe-$(date +'%Y-%m-%d.%H')-revpi-buster.log"
-sudo mv armhf-revpi.xml "/mnt/wshare/elbe-images/elbe-$(date +'%Y-%m-%d.%H')-revpi-buster.xml"
